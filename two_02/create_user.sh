b95fdd69b47610ac98870cecb18b71f476f33379
vagrant <vagrant@vagrant.vm>
2024-06-20 12:38:50 +0000

    Resolved merge conflicts

---
+++ two_02/create_user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter username: " username
+
+echo "you entered $username"
+
+sudo useradd -m $username
+
+echo "New User added"#!/bin/bash
+
+read -p "Enter username: " username
+
+echo "you entered $username"
+
+sudo useradd -m $username
+
+echo "New User added"
+
---
+++ two_02/variable.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This is ag5
+
+<< comment
+Anything 
+written 
+here will not be execute
+comment
+
+name="vinodi"
+
+echo "Name is $name, and date is $(date)"
+
+echo "enter the name:"
+
+read username
+
+echo "You entered $username"
+
+echo "The characters in $0 are:  $1 $2"
